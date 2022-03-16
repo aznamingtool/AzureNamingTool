7b1baabf4ba996787623fea40287418890497c65
Bryan Soltis <brsoltis@microsoft.com>
2022-03-16 08:58:06 -0500

    Updated admin function to isolate to indivudal

---
+++ Helpers/SessionHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace AzNamingTool.Helpers
+{
+    public class SessionHelper
+    {
+        private ProtectedSessionStorage storage;
+
+        public SessionHelper(ProtectedSessionStorage storage)
+        {
+            this.storage = storage;
+        }
+
+        public async Task<object> GetSessionValue(string name)
+        {
+            return await storage.GetAsync<object>(name);
+        }
+
+        public async Task SetSessionValue(string name, object value)
+        {
+            await storage.SetAsync(name, value);
+        }
+
+    }
+}
